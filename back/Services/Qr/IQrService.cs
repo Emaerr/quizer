9f205117c0ffab956b64538a50ce3ca9fd708051
KrauzerKrip <krauzerkrip@gmail.com>
2024-05-15 17:24:18 +0300

    feat: add qr code service

---
+++ back/Services/Qr/IQrService.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+
+namespace Quizer.Services.Qr
+{
+    public interface IQrService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Result with QR code png image byte array or error</returns>
+        public Result<byte[]> GetQrByName(string name);
+    }
+}
---
+++ back/Services/Qr/QrService.cs
@@ -0,0 +1,45 @@
+﻿using FluentResults;
+using QRCoder;
+using System.Drawing.Imaging;
+using System.Drawing;
+using Quizer.Exceptions.Services;
+using QRCoder.Exceptions;
+using System.IO;
+
+namespace Quizer.Services.Qr
+{
+    public class QrService : IQrService, IQrGenerationService
+    {
+        private Dictionary<string, byte[]> _qrCodes = [];
+
+        public Result<byte[]> GetQrByName(string name)
+        {
+            _qrCodes.TryGetValue(name, out var result);
+
+            if (result == null)
+            {
+                return Result.Fail(new QrNotFoundError($"QR code {name} not found."));
+            }
+
+            return result;
+        }
+
+        public void GenerateQrCode(string name, string str)
+        {
+            using QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            try
+            {
+                using QRCodeData qrCodeData = qrGenerator.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q);
+                using PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+                byte[] qrCodeImage = qrCode.GetGraphic(20);
+                _qrCodes[name] = qrCodeImage;
+                File.WriteAllBytes(@"C:\Users\User\Desktop\yourfile.png", qrCodeImage);
+            }
+            catch (DataTooLongException e)
+            {
+                throw new QrGenerationFailException($"QR {name} generation failed ", e);
+
+            }
+        }
+    }
+}
