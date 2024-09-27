bbd9070c165bb0db78a409cc22e19a5eb91256f6
northarg <es.maybepsh@gmail.com>
2024-09-27 19:42:13 +0300

    Update desktop44.js

--- front/resource/js/desktop44.js
+++ front/resource/js/desktop44.js
@@ -24,7 +24,8 @@ function fetchNextQuestion() {
     .then(response => response.json())
     .then(data => {
         console.log('Данные получены:', data);
-        loadNextQuestion(data);
+        const nextQuestion = Array.isArray(data) ? data[0] : data;
+        loadNextQuestion(nextQuestion);
     })
     .catch((error) => {
         console.error('Ошибка при получении данных:', error);
@@ -33,10 +34,11 @@ function fetchNextQuestion() {
 
 function loadNextQuestion(data) {
     document.getElementById("questionText").textContent = data.question;
-    const buttons = document.querySelectorAll("option");
+    const buttons = document.querySelectorAll(".option");
     buttons.forEach((button, index) => {
         button.textContent = data.answers[index];
         button.setAttribute("data-answer", data.answers[index]);
+        console.log(buttons)
     });
     resetTimer();
 }
